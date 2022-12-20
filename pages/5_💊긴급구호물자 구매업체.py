03c985a01de42724f9f28a29adfa1f7a02e6dd8b
sangwook <wjddm3@naver.com>
2022-12-20 11:09:23 +0900

    top bar add

--- pages/5_💊긴급구호물자 구매업체.py
+++ pages/5_💊긴급구호물자 구매업체.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
+from streamlit_option_menu import option_menu
 
 filePath, fileName = os.path.split(__file__)
 data_path = os.path.join(filePath,'using_data','구호물자정보.csv')
@@ -11,6 +12,10 @@ st.set_page_config(
     layout = 'wide'
 )
 
+selected2 = option_menu(None, ["🙄Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+
 st.header("💊긴급구호물자 구매업체")
 st.write("위치 정보를 선택하여 가까운 구매업체를 찾으세요🙏")
 df = pd.read_csv(data_path)
@@ -19,3 +24,5 @@ cd_nm = st.selectbox('시도 선택',list(df['시도명'].unique()))
 sgg_nm = st.selectbox('시군구 선택',list(df[df['시도명'] == cd_nm]['시군구명'].unique()))
 df = df[(df['시도명'] == cd_nm) & (df['시군구명'] == sgg_nm)]
 st.dataframe(data= df.reset_index(drop = True), use_container_width= True)
+
+
