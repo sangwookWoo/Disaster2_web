9c84d5fe5613f41bc0678c7f6e41f98fedd2b87a
sangwook <wjddm3@naver.com>
2022-12-20 18:35:06 +0900

    JHI

--- pages/1_🌊홍수관련 실시간 정보.py
+++ pages/1_🌊홍수관련 실시간 정보.py
@@ -80,7 +80,7 @@ def dam_data_make():
 def dam_map(data):
         m = folium.Map(
         location=[data['lat'].mean(), data['lon'].mean()],
-        zoom_start= 7, width = '70%', height = '50%', scrollWheelZoom=False, dragging=False
+        zoom_start= 7, width = '70%', height = '50%'
         )
         coords = data[['lat', 'lon', 'obsnm', 'swl', 'inf', 'sfw', 'ecpc', 'tototf']]
         for idx in coords.index:
@@ -103,7 +103,7 @@ def bo_data_make():
 def bo_map(data):
         m = folium.Map(
         location=[data['lat'].mean(), data['lon'].mean()],
-        zoom_start= 7, width = '70%', height = '50%',  scrollWheelZoom=False, dragging=False
+        zoom_start= 7, width = '70%', height = '50%'
         )
         coords = data[['lat', 'lon', 'obsnm', 'swl', 'inf', 'sfw', 'ecpc', 'tototf']]
         for idx in coords.index:
